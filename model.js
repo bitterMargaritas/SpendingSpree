e09ab77bbb94580caeb9197f75f4dce500303bb1
LoreenRH <lorena.rangel@docker.com>
2018-07-23 22:27:51 +0200

    wip: add the database
    Signed-off-by: LoreenRH <lorena.rangel@docker.com>

---
+++ model.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var UserSchema = new Schema({
+    name: {
+        type: String,
+        required: "Fill name, or die"
+    },
+    amountOwed: {
+        type: int,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('Users', UserSchema)
--- router.js
+++ router.js
@@ -1,12 +1,12 @@
 
 module.exports = function(app) {
-    app.route('/test')
+    app.route('/users')
         .get(function (req, res) {
             console.log(req.route)
             res.json({ response: 'a GET request for LOOKING at questions' })
         })
     
-    app.route('/test/:user')
+    app.route('/users/:user')
         .get(function (req, res) {
             console.log(req.route)
             res.json({ response: req.params.user })
