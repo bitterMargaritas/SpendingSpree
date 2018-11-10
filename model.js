bf752887ae22d56b0f4a96a651549bedf828da9e
LoreenRH <lorena.rangel@docker.com>
2018-11-10 12:17:51 +0100

    update model
    Signed-off-by: LoreenRH <lorena.rangel@docker.com>

--- model.js
+++ model.js
@@ -7,9 +7,43 @@ var UserSchema = new Schema({
         required: "Fill name, or die"
     },
     amountOwed: {
+        type: Object,
+        default: null
+        // should be "name": String, "amount": number
+    },
+    actions: []
+})
+
+var actionSchema = new Schema({
+    action: {
+        type: String,
+        required: "enter action name, or die"
+    },
+    date: {
+        type: Date,
+        required: "enter date or suffer"
+    },
+    mainParticipant: {
+        type: String,
+        required: "fill name..."
+    },
+    participants: {
+        type: Array,
+        default: []
+    },
+    typeOfSplit: {
+        type: String,
+        default: "equalSplit"
+    },
+    amount: {
         type: Number,
-        default: 0
+        required: ""
+    },
+    notes: {
+        type: "String",
+        default: ""
     }
+
 })
 
 module.exports = mongoose.model('Users', UserSchema)
